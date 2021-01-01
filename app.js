8517c22811d8e7c4bead9019764e62b8d456dedc
Dagger Silver <shitcodder@gmail.com>
2021-01-01 10:16:21 +0530

    d

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const mongoose = require("mongoose");
 const path = require("path");
-const mongoURL = "mongodb://localhost/membersOnly"
+const mongoURL = `mongodb+srv://dagger:${process.env.password}@cluster0.s6okb.mongodb.net/${process.env.dbname}?retryWrites=true&w=majority`;
 const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 const flash = require('express-flash');
